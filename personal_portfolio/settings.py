f1eb74dc4ae4c09bfb8c9a7bbe7ceec7787470ff
EmmanuelSolorzano <70496324+EmmanuelSolorzano@users.noreply.github.com>
2023-03-15 22:18:38 -0600

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bx_snz1^8mud+tto=7p5p^keya!v*t5^o$aubh-t_la4ii=czp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.alwaysdata.net','www.emmanuelsolorzano.com']
+ALLOWED_HOSTS = ['emmanuelsolorzano.alwaysdata.net','www.emmanuelsolorzano.com']
 
 # Application definition
 
